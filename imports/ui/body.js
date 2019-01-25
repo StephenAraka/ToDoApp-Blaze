f37ddb252392df892575f45639b5513e3663cd4a
StephenAraka <stevenaraka@gmail.com>
2019-01-25 15:16:08 +0300

    Add forms and events

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,6 +5,25 @@ import './body.html';
  
 Template.body.helpers({
   tasks() {
-      return Tasks.find({});
+      return Tasks.find({}, { sort: { createdAt: -1 } });
   },
+});
+
+Template.body.events({
+    "submit .new-task" (event) {
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // insert new task into the colletion
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        });
+
+        // clear the form
+        target.text.value = "";
+    },
 });
